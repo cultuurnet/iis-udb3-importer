1b373ae0ee5907e75882ce1912438984b37ca187
Jonas Verhaeghe <jonas@cultuurnet.be>
2016-10-10 19:59:25 +0200

    III-1066 add tests for wrong version

---
+++ tests/Exceptions/UnexpectedNamespaceExceptionTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace CultuurNet\UDB3\IISImporter\Exceptions;
+
+use CultuurNet\UDB3\IISImporter\Event\ParserV3;
+
+class UnexpectedNamespaceExceptionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ParserV3
+     */
+    protected $parser;
+
+    public function setUp()
+    {
+        $this->parser = new ParserV3();
+    }
+
+    /**
+     * @test
+     */
+    public function itThrowsTheExpectedException()
+    {
+        $faultyXml = file_get_contents(__DIR__ . '/../NotACdbxml.xml');
+        $this->setExpectedException(UnexpectedNamespaceException::class);
+        $this->parser->loadDOM($faultyXml);
+    }
+}
---
+++ tests/Exceptions/UnexpectedRootElementExceptionTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace CultuurNet\UDB3\IISImporter\Exceptions;
+
+use CultuurNet\UDB3\IISImporter\Event\ParserV3;
+
+class UnexpectedRootElementExceptionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ParserV3
+     */
+    protected $parser;
+
+    public function setUp()
+    {
+        $this->parser = new ParserV3();
+    }
+
+    /**
+     * @test
+     */
+    public function itThrowsTheExpectedException()
+    {
+        $faultyXml = file_get_contents(__DIR__ . '/../RootlessVersion3.xml');
+        $this->setExpectedException(UnexpectedRootElementException::class);
+        $this->parser->loadDOM($faultyXml);
+    }
+}
