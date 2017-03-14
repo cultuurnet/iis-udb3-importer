2a2aa56d6110ab373736ebe3f62619e4be0c0dc8
Luc Wollants <luc.wollants@apptite.be>
2017-03-14 16:02:28 +0100

    III-1162 Added unit test for AMQPPropertiesFactory.

---
+++ tests/AMQP/AMQPPropertiesFactoryTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace CultuurNet\UDB3\IISImporter\AMQP;
+
+use PhpAmqpLib\Message\AMQPMessage;
+
+class AMQPPropertiesFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var AMQPPropertiesFactory
+     */
+    private $amqpPropertiesFactory;
+
+    /**
+     * @var array
+     */
+    private $expectedProperties;
+
+    protected function setUp()
+    {
+        $this->amqpPropertiesFactory = new AMQPPropertiesFactory();
+
+        $this->expectedProperties = [
+            'delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT
+        ];
+    }
+
+    /**
+     * @test
+     */
+    public function it_creates_properties()
+    {
+        $properties = $this->amqpPropertiesFactory->createProperties(false);
+
+        $this->expectedProperties['content_type'] = 'application/vnd.cultuurnet.udb2-events.event-created+json';
+
+        $this->assertEquals($this->expectedProperties, $properties);
+    }
+
+    /**
+     * @test
+     */
+    public function it_creates_properties_for_update()
+    {
+        $properties = $this->amqpPropertiesFactory->createProperties(true);
+
+        $this->expectedProperties['content_type'] = 'application/vnd.cultuurnet.udb2-events.event-updated+json';
+
+        $this->assertEquals($this->expectedProperties, $properties);
+    }
+}
