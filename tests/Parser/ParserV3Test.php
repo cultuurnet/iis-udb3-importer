12285a8495329a57ca6622f24f3d9a2d96008d43
Luc Wollants <luc.wollants@apptite.be>
2017-03-20 20:31:30 +0100

    III-1159 Added unit test for splitting an xml file.

--- tests/Parser/ParserV3Test.php
+++ tests/Parser/ParserV3Test.php
@@ -49,4 +49,20 @@ class ParserV3Test extends \PHPUnit_Framework_TestCase
         $valid = file_get_contents(__DIR__ . '/../Binary.xml');
         $this->assertFalse($this->parser->validate($valid));
     }
+
+    /**
+     * @test
+     */
+    public function it_can_split_a_file()
+    {
+        $largeFile = file_get_contents(__DIR__ . '/../FileToSplit.xml');
+        $events = $this->parser->split($largeFile);
+
+        $this->assertCount(5, $events);
+        $this->assertArrayHasKey('EGD201711542', $events);
+        $this->assertArrayHasKey('EGD201711555', $events);
+        $this->assertArrayHasKey('EGD201711557', $events);
+        $this->assertArrayHasKey('EGD201711574', $events);
+        $this->assertArrayHasKey('EGD201711576', $events);
+    }
 }
