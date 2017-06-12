3431c34340421950964dd3209b907ff6aad67668
Jonas Verhaeghe <jonas@localhost.localdomain>
2017-06-12 16:41:35 +0200

    feature/IMP-26

---
+++ tests/CategorizationRules/FlandersRegionTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CultuurNet\UDB3\IISImporter\CategorizationRules;
+
+use CultuurNet\UDB3\IISImporter\Download\DownloaderInterface;
+use CultuurNet\UDB3\IISImporter\Url\UrlFactoryInterface;
+use League\Flysystem\Adapter\AbstractAdapter;
+use ValueObjects\Web\Url;
+
+class FlandersRegionTest extends \PHPUnit_Framework_TestCase
+{
+
+}
